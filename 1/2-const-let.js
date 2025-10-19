58f1c5c3a4765394907ce7968099369818f4853e
hgti32 <127024694+hgti32@users.noreply.github.com>
2025-10-19 19:33:11 +0300

    1

---
+++ 1/2-const-let.js
@@ -0,0 +1,13 @@
+'use strict';
+
+let number = 2025;
+console.log(number); //2025
+console.dir({ number }); //{number: 2025}
+
+const name = 'Marko'; //Marko
+let age = 17; // 17
+const city = 'Kyiv'; // Kyiv
+console.log({ name, age, city }); //{ name: 'Marko', age: 17, city: 'Kyiv' }
+
+
+
